f93c5236c579ea4b0bfa97624885c3ab53b5a165
balakelly459 <21192911@CFP127ALUNO.LOCAL>
2021-10-01 11:24:32 -0300

    icatalogo

--- categorias/acoes.php
+++ categorias/acoes.php
@@ -1,7 +1,21 @@
 <?php
-
+session_start();
 /*CONEXÃO COM BANCO DE DADOS*/
 require('../database/conexao.php');
+
+//FUNÇÃO DE VALIDAÇÃO
+function validaCampos(){
+
+    $erros = [];
+
+    if(isset($_POST['descricao']) || $_POST['descricao'] == ""){
+
+        $erros[] = "O campo descrição é de preenchimento obrigatório";
+    }
+
+    return $erros;
+
+}
 
 //TRATAMENTO DE DADOS VINDOS DO FORMULÁRIO
 
@@ -10,6 +24,21 @@ require('../database/conexao.php');
 
 switch ($_POST['acao']) {
     case 'inserir':
+
+        //CHAMADA DA FUNÇÃO DE VALIDAÇÃO DE ERROS:
+        $erros = validaCampos();
+
+        //VERIFICAR SE EXISTEM ERROS:
+        if(count($erros) > 0){
+
+            $_SESSION["erros"] = $erros;
+
+            header('location: index.php');
+
+            exit();
+        }
+
+
         // echo 'INSERIR';exit;
         $descricao = $_POST['descricao'];
 
@@ -33,6 +62,19 @@ switch ($_POST['acao']) {
 
 
         break;
+
+        case 'deletar': 
+            
+            $categoriaID = $_POST['categoriaId'];
+
+            $sql = "DELETE FROM tbl_categoria WHERE id= $categoriaID";
+
+            $resultado = mysqli_query($conexao, $sql);
+
+            header('location: index.php');
+            
+            
+            break;
     
     default:
         # code...
--- categorias/editar.php
+++ categorias/editar.php
@@ -1,17 +1,10 @@
 <?php
 
-session_start();
+require('../database/conexao.php');
 
-require ('../database/conexao.php');
+$idCategoria = $_GET['id'];
+$sql = "SELECT * FROM tbl_categoria WHERE id = $idCategoria"
 
-$sql = "SELECT * FROM tbl_categoria";
-
-$resultado = mysqli_query($conexao, $sql);
-
-// echo '<pre>';
-// var_dump($resultado);
-// echo '</pre>';
-// exit;
 
 ?>
 
@@ -37,8 +30,8 @@ $resultado = mysqli_query($conexao, $sql);
         <section class="categorias-container">
             <main>
                 <form class="form-categoria" method="POST" action="./acoes.php">
-                    <input type="hidden" name="acao" value="inserir" />
-                    <h1 class="span2">Adicionar Categorias</h1>
+                    <input type="hidden" name="acao" value="editar" />
+                    <h1 class="span2">Editar Categorias</h1>
                     <ul>
                         <?php 
 
@@ -68,38 +61,13 @@ $resultado = mysqli_query($conexao, $sql);
                         <input type="text" name="descricao" id="descricao"/>
                     </div>
                     <button type="button" onclick="javascript:window.location.href = '../produtos/'">Cancelar</button>
-                    <button>Salvar</button>
+                    <button>Editar</button>
                 </form>
-                <h1>Lista de Categorias</h1>
-
-                <?php
-
-                while($categoria = mysqli_fetch_array($resultado)){
-
-
-                ?>
-
-                    <div class="card-categorias">
-                        <?php echo $categoria["descricao"]; ?>
-                        <img onclick="deletar(<?php  echo $categoria['id']; ?>)" src="https://icons.veryicon.com/png/o/construction-tools/coca-design/delete-189.png" />
-                        <img onclick="javascript: window.location= 'editar.php?id= <?php  echo $categoria['id']; ?>' " src="https://icons.veryicon.com/png/o/leisure/weight-with-linear-icon/edit-65.png" />
-                    </div>
-
-                    <?php } ?>
-
-                <form id="form-deletar" method="POST" action="./acoes.php">
-                    <input type="hidden" name="acao" value="deletar" />
-                    <input type="hidden" id="categoriaId" name="categoriaId" value="" />
-                </form>
+               
             </main>
         </section>
     </div>
-    <script lang="javascript">
-        function deletar(categoriaId){
-            document.querySelector("#categoriaId").value = categoriaId;
-            document.querySelector("#form-deletar").submit();
-        }
-    </script>
+
 </body>
 
 </html>
